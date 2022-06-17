1a1d4ed59fa5a0b094fdb87fbf0e5a3fc17d8411
German Istomin <needwolf@gmail.com>
2022-06-17 20:22:46 +1000

    add posts method for pg

--- pkg/storage/postgres/postgres.go
+++ pkg/storage/postgres/postgres.go
@@ -1,1 +1,87 @@
 package postgres
+
+import (
+	"GoNews/pkg/storage"
+	"context"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+// Хранилище данных.
+type Store struct {
+	db *pgxpool.Pool
+}
+
+// Конструктор объекта хранилища.
+func New(connectionString string) (*Store, error) {
+	db, err := pgxpool.Connect(context.Background(), connectionString)
+	if err != nil {
+		return nil, err
+	}
+	s := Store{
+		db: db,
+	}
+	return &s, nil
+}
+
+func (s *Store) Posts() ([]storage.Post, error) {
+	rows, err := s.db.Query(context.Background(), `
+		SELECT
+			p.id,
+			p.title,
+			p.content,
+			p.author_id,
+			a.name,
+			p.created_at,
+			p.published_at
+		FROM posts p, authors a
+		WHERE
+			p.author_id = a.id
+		ORDER BY id;
+	`,
+	)
+	if err != nil {
+		return nil, err
+	}
+	var posts []storage.Post
+	for rows.Next() {
+		var p storage.Post
+		err = rows.Scan(
+			&p.ID,
+			&p.Title,
+			&p.Content,
+			&p.AuthorID,
+			&p.AuthorName,
+			&p.CreatedAt,
+			&p.PublishedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, p)
+	}
+	return posts, rows.Err()
+}
+
+func (s *Store) AddPost(storage.Post) error {
+	return nil
+}
+func (s *Store) UpdatePost(storage.Post) error {
+	return nil
+}
+func (s *Store) DeletePost(storage.Post) error {
+	return nil
+}
+
+/*var posts = []storage.Post{
+	{
+		ID:      1,
+		Title:   "Effective Go",
+		Content: "Go is a new language. Although it borrows ideas from existing languages, it has unusual properties that make effective Go programs different in character from programs written in its relatives. A straightforward translation of a C++ or Java program into Go is unlikely to produce a satisfactory result—Java programs are written in Java, not Go. On the other hand, thinking about the problem from a Go perspective could produce a successful but quite different program. In other words, to write Go well, it's important to understand its properties and idioms. It's also important to know the established conventions for programming in Go, such as naming, formatting, program construction, and so on, so that programs you write will be easy for other Go programmers to understand.",
+	},
+	{
+		ID:      2,
+		Title:   "The Go Memory Model",
+		Content: "The Go memory model specifies the conditions under which reads of a variable in one goroutine can be guaranteed to observe values produced by writes to the same variable in a different goroutine.",
+	},
+}*/
